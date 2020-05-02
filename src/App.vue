8564c0e754234cb2b70fd19e78ec17d7804b2f32
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-02 15:17:52 +0100

    Used v-model to connect form fields with data properties, method submit added

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,18 @@
           <form>
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="user.email">
               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              <label for="nickname">Nickname</label>
+              <input type="text" class="form-control" id="nickname" placeholder="Enter nickname" v-model="user.nickname">
             </div>
             <div class="form-group form-check">
               <input type="checkbox" class="form-check-input" id="exampleCheck1">
               <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" @click.prevent="submit">Submit</button>
           </form>
         </div>
       </div>
@@ -30,13 +30,23 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      user: {
+        email: '',
+        nickname: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      return console.log("Nickname is " + this.user.nickname + ", and email is " + this.user.email)
+    }
+  }
 }
 </script>
 
 <style>
-.container{
-  
-}
 h1 {
   text-align: center;
   height: 200px;
